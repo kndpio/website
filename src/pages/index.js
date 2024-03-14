61aa8cb523918f2ca45f69a16805c37901c7975e
lucian <burlacu.lucian03@gmail.com>
2024-03-14 18:07:02 +0200

    Cleanup home page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,33 @@
 import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-// import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import VideoTeaser from "../components/VideoTeaser";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+  const titleParts = [
+    { text: "Generate " },
+    { text: "more leads", color: "theme" },
+    { text: " with social media marketing" },
+  ];
+  const subtitleParts = [
+    {
+      text: "Mauris donec ociis diam magnis sapien sagittis sapien tempor volute gravida ",
+    },
+    { text: "aliquet tortor undo aliquet an egestas", color: "black" },
+    { text: " in magna egestas sapien quaerat" },
+  ];
   return (
-    // <Layout title={`${siteConfig.title}`}>
     <Layout>
       <main>
         <HomepageFeatures />
       </main>
       <VideoTeaser
-        logoSrc="/images/square-logo.png"
-        title="Generate more leads with social media marketing"
-        subtitle="Mauris donec ociis diam magnis sapien sagittis sapien tempor volute gravida aliquet tortor undo aliquet an egestas in magna egestas sapien quaerat"
+        titleParts={titleParts}
+        subtitleParts={subtitleParts}
         videoSrc="https://www.youtube.com/embed/SZEflIVnhH8"
-        imageSrc="/images/dashboard-03.png"
+        imageSrc="/images/testGif.gif"
       />
     </Layout>
   );
