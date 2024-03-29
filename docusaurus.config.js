b8280e49d3605df32634def3b1e65ff172074134
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2024-03-29 12:56:52 +0200

    added tab title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: ' ',
+  title: 'KNDP',
   tagline: '',
   favicon: '/img/logo.png',
 
